9243e70fc8397e430d5502ea3be90d0ca16c28c9
nirbhay singh <nirbhaysingh281@gmail.com>
2022-03-30 16:41:07 +0530

    projectnamechange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Let The Free Times Roll';
+  title = 'Codel';
   date = new Date().getFullYear();
 }
